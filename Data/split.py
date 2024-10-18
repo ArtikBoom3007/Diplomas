f176eba2620b32306e37fda5436fd10c0e4c9d0b
Artik BoOm <ikrav514@gmail.com>
2024-10-18 15:09:34 +0300

    Add metadata to split.py

--- Data/split.py
+++ Data/split.py
@@ -3,9 +3,11 @@ from collections import Counter
 import os
 import sys
 from sklearn.model_selection import train_test_split
+import pickle
 
 # Подгружаем пути к директориям с нашими алгоритмами:
 script_path = os.path.join(os.getcwd(), "../Scripts/")
+print(script_path)
 sys.path.append(script_path)
 import data_generator as dgen
 
@@ -15,55 +17,81 @@ balance_by_train_test = 1
 def split(X, y, strategy : int, test_size = 0.2):
     match strategy:
         case 0:
-            class_counts = Counter(y)
+            class_counts = Counter(y[0])
             min_class_size = min(class_counts.values())  # Находим минимальное количество классов для балансировки
 
             # Создаем равномерную выборку (по минимальному количеству примеров каждого класса)
             balanced_signals = []
-            balanced_labels = []
+            balanced_labels = [[], []]
             for label in class_counts.keys():
-                indices = np.where(y == label)[0]
+                indices = np.where(y[0] == label)[0]
                 selected_indices = np.random.choice(indices, min_class_size, replace=False)
                 balanced_signals.extend([X[i] for i in selected_indices])
-                balanced_labels.extend([y[i] for i in selected_indices])
+                balanced_labels[0].extend([y[0][i] for i in selected_indices])
+                balanced_labels[1].extend([y[1][i] for i in selected_indices])
 
-            # Преобразуем списки обратно в numpy объекты для сохранения
-            balanced_signals = np.array(balanced_signals, dtype=object)
-            balanced_labels = np.array(balanced_labels)
+            # Создаем структуру для одновременного хранения сигналов, меток и метаданных
+            data = list(zip(balanced_signals, balanced_labels[0], balanced_labels[1]))
 
-            # Разделение на обучающую и тестовую выборки
-            X_train, X_test, y_train, y_test = train_test_split(
-                balanced_signals, balanced_labels, test_size=test_size, stratify=balanced_labels, random_state=42
+            # Преобразуем в массив объектов, если необходимо
+            data = np.array(data, dtype=object)
+
+            # Разделяем данные на train и test с сохранением структуры
+            train_data, test_data = train_test_split(
+                data, test_size=test_size, stratify=balanced_labels[0], random_state=42
             )
 
+            # Разворачиваем обратно сигналы, метки и метаданные
+            X_train, y_train_labels, y_train_metadata = zip(*train_data)
+            X_test, y_test_labels, y_test_metadata = zip(*test_data)
+
+            y_train = [y_train_labels, y_train_metadata]
+            y_test = [y_test_labels, y_test_metadata]
+
             # Проверим распределение классов в обучающей и тестовой выборке
-            unique_train, counts_train = np.unique(y_train, return_counts=True)
-            unique_test, counts_test = np.unique(y_test, return_counts=True)
+            unique_train, counts_train = np.unique(y_train_labels, return_counts=True)
+            unique_test, counts_test = np.unique(y_test_labels, return_counts=True)
 
             print(f"Train class distribution: {dict(zip(unique_train, counts_train))}")
             print(f"Test class distribution: {dict(zip(unique_test, counts_test))}")
 
         case 1:
-            # Стратифицированное разбиение на обучающую и тестовую выборки
-            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, stratify=y, random_state=42)
+            data = list(zip(X, y[0], y[1]))
+
+            # Преобразуем в массив объектов, если необходимо
+            data = np.array(data, dtype=object)
+
+            # Разделяем данные на train и test с сохранением структуры
+            train_data, test_data = train_test_split(
+                data, test_size=test_size, stratify=y[0], random_state=42
+            )
+
+            # Разворачиваем обратно сигналы, метки и метаданные
+            X_train, y_train_labels, y_train_metadata = zip(*train_data)
+            X_test, y_test_labels, y_test_metadata = zip(*test_data)
+
+            y_train = [y_train_labels, y_train_metadata]
+            y_test = [y_test_labels, y_test_metadata]
 
             # Проверим распределение классов в обучающей и тестовой выборке
-            unique_train, counts_train = np.unique(y_train, return_counts=True)
-            unique_test, counts_test = np.unique(y_test, return_counts=True)
+            unique_train, counts_train = np.unique(y_train[0], return_counts=True)
+            unique_test, counts_test = np.unique(y_test[0], return_counts=True)
 
             print(f"Train class distribution: {dict(zip(unique_train, counts_train))}")
             print(f"Test class distribution: {dict(zip(unique_test, counts_test))}")
 
-            X_train = np.array(X_train, dtype=object)
-            X_test = np.array(X_test, dtype=object)
-
 
 
     # Сохранение данных в файлы numpy
-    np.save('dumped/X_train.npy', X_train)
-    np.save('dumped/y_train.npy', y_train)
-    np.save('dumped/X_test.npy', X_test)
-    np.save('dumped/y_test.npy', y_test)
+    with open('dumped/X_train.pkl', 'wb') as f:
+        pickle.dump(X_train, f)
+    with open('dumped/y_train.pkl', 'wb') as f:
+        pickle.dump(y_train, f)
+    with open('dumped/X_test.pkl', 'wb') as f:
+        pickle.dump(X_test, f)
+    with open('dumped/y_test.pkl', 'wb') as f:
+        pickle.dump(y_test, f)
+
 
     print("Данные успешно сохранены!")
 
@@ -78,23 +106,28 @@ if __name__ == "__main__":
                 args["highcut"] = hc
     
 
-    channels = input("Input channels divided by comma: ")
-    ch = [part for part in channels.split(',') if part.strip()]
+    # channels = input("Input channels divided by comma: ")
+    # ch = [part for part in channels.split(',') if part.strip()]
 
-    if channels:
-        args["channel"] = ch
+    # if channels:
+    #     args["channel"] = ch
 
     dgen.init(*args)
 
-    amy, amyc, norm = dgen.read_data()
+    amy, amyc, norm, amy_h, amyc_h, norm_h = dgen.read_data_with_meta()
 
     strat = input("strategy 0 - balance by class \nstrategy 1 - balance by train test set\nEnter balance strategy: ")
+    if strat == '':
+        strat = 0
+
     test_sz = input("Enter test size: (default 0.2): ")
 
     tts_args = {}
 
     X = norm + amyc + amy
     y = np.concatenate([np.zeros(len(norm) + len(amyc)), np.ones(len(amy))])
+    meta = np.concatenate([norm_h, amyc_h, amy_h])
+    y = np.stack([y, meta], axis=0)
     
     tts_args["X"] = X
     tts_args["y"] = y
@@ -103,6 +136,4 @@ if __name__ == "__main__":
         tts_args["test_size"] = test_sz
 
     split(**tts_args)
-
-    
 
--- Scripts/data_generator.py
+++ Scripts/data_generator.py
@@ -30,7 +30,7 @@ def highpass(highcut, order, fs):
 def final_filter(data, fs, order=4):
     if make_filter:
         b, a = highpass(highcut, order, fs)
-        x = lfilter(b, a, data)
+        x = lfilter(b, a, data, axis=1)
         return x
     return data
 
@@ -55,23 +55,65 @@ def read_data():
     for name in os.listdir(amy_path):
         signals, signal_headers, _ = highlevel.read_edf(os.path.join(amy_path, name), ch_names=ecg_channel)
 
-        signal = signals[0]
-        amy.append(final_filter(signal, signal_headers[0]["sample_frequency"]))  
+        amy.append(final_filter(signals, signal_headers[0]["sample_frequency"]))
 
     for name in os.listdir(amyc_path):
         signals, signal_headers, _ = highlevel.read_edf(os.path.join(amyc_path, name), ch_names=ecg_channel)
 
-        signal = signals[0]
-        amyc.append(final_filter(signal, signal_headers[0]["sample_frequency"]))  
+        amyc.append(final_filter(signals, signal_headers[0]["sample_frequency"]))
 
     for name in os.listdir(norm_path):
         signals, signal_headers, _ = highlevel.read_edf(os.path.join(norm_path, name), ch_names=ecg_channel)
 
-        signal = signals[0]
-        norm.append(final_filter(signal, signal_headers[0]["sample_frequency"]))  
+        norm.append(final_filter(signals, signal_headers[0]["sample_frequency"]))
 
     sample_rate = signal_headers[0]["sample_frequency"]
     return amy, amyc, norm
+
+def read_data_with_meta():
+    global sample_rate, amy_path, amyc_path, norm_path
+    
+    # If this module starts from different places. Make path consistency.
+    path = os.path.join(module_path, "../Data")
+
+    amy_path = os.path.join(path, "Amy/Amy")
+    amyc_path = os.path.join(path, "AmyC/AmyC")
+    norm_path = os.path.join(path, "AMY_add/AMY/2")
+
+    amy = []
+    amyc = []
+    norm = []
+
+    amy_header = []
+    amyc_header = []
+    norm_header = []
+
+    #signals, signal_headers, header = highlevel.read_edf(os.path.join(amy_path, "Amy1.edf"), ch_names=['ECG I'])
+    # print(signal_headers)
+    # print(header)
+
+    for name in os.listdir(amy_path):
+        signals, signal_headers, _ = highlevel.read_edf(os.path.join(amy_path, name))
+
+        amy.append(final_filter(signals, signal_headers[0]["sample_frequency"]))
+        amy_header.append(_)
+
+    for name in os.listdir(amyc_path):
+        signals, signal_headers, _ = highlevel.read_edf(os.path.join(amyc_path, name))
+
+        amyc.append(final_filter(signals, signal_headers[0]["sample_frequency"]))
+        amyc_header.append(_)
+
+    for name in os.listdir(norm_path):
+        signals, signal_headers, _ = highlevel.read_edf(os.path.join(norm_path, name))
+
+        norm.append(final_filter(signals, signal_headers[0]["sample_frequency"]))
+        norm_header.append(_)
+
+    sample_rate = signal_headers[0]["sample_frequency"]
+    return amy, amyc, norm, amy_header, amyc_header, norm_header
+
+
 
 def crop(data : list):
     parts = []
